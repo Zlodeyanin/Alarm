cdeccbd978629dcd7df91d19ec933aa33d2f18b4
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2023-08-23 13:14:17 +0300

    002 - Fix bags

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,34 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class Movement : MonoBehaviour
 {
+    private const string _animatorParameter = "Speed";
+
     private Animator _animator;
+    private SpriteRenderer _flip;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _flip = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        const string AnimatorParameter = "Speed";
-        var flip = gameObject.GetComponent<SpriteRenderer>();
+        float playerGo = 2f;
+        float playerStay = 0;
+        float playerGoLeft = -0.005f;
+        float playerGoRight = 0.005f;
+        float playerTransformY = 0;
+        float playerTransformZ = 0;
 
         if (Input.GetKey(KeyCode.A))
         {
-            flip.flipX = false;
-            transform.Translate(-0.005f, 0, 0);
-            _animator.SetFloat(AnimatorParameter, 2);
+            _flip.flipX = false;
+            transform.Translate(playerGoLeft, playerTransformY, playerTransformZ);
+            _animator.SetFloat(_animatorParameter, playerGo);
         }
         else if (Input.GetKey(KeyCode.D))
-        {            
-            flip.flipX = true;
-            transform.Translate( 0.005f, 0, 0);
-            _animator.SetFloat(AnimatorParameter, 2);
+        {
+            _flip.flipX = true;
+            transform.Translate(playerGoRight, playerTransformY, playerTransformZ);
+            _animator.SetFloat(_animatorParameter, playerGo);
         }
         else
         {
-            _animator.SetFloat(AnimatorParameter, 0);
+            _animator.SetFloat(_animatorParameter, playerStay);
         }
     }
 }
